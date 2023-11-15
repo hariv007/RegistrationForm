6a0761aa7235e8a25ccaa8e09e15d0966e62ab86
hari <hari@sociusus.com>
2023-11-15 11:13:34 +0530

    Initial commit with multiple courses adding functionality

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultipleCourses.Data;
+using MultipleCourses.Models;
+using Newtonsoft.Json;
+
+namespace MultipleCourses.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public RegisterController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Save()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Save(string jsonData)
+        {
+            try
+            {
+                RegistrationForm data = JsonConvert.DeserializeObject<RegistrationForm>(jsonData);
+                _dbContext.Add(data);
+                await _dbContext.SaveChangesAsync();
+                return Json(new { success = true });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Migrations/20231109125122_Initial.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultipleCourses.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "registrationForms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_registrationForms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "qualification",
+                columns: table => new
+                {
+                    QualificationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearofPass = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    University = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RegistrationFormId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_qualification", x => x.QualificationId);
+                    table.ForeignKey(
+                        name: "FK_qualification_registrationForms_RegistrationFormId",
+                        column: x => x.RegistrationFormId,
+                        principalTable: "registrationForms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_qualification_RegistrationFormId",
+                table: "qualification",
+                column: "RegistrationFormId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "qualification");
+
+            migrationBuilder.DropTable(
+                name: "registrationForms");
+        }
+    }
+}
