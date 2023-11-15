db01cb34feeb88f2de54f9fc967bee18bb37264d
hari <hari@sociusus.com>
2023-11-15 11:13:34 +0530

    Initial commit with multiple courses adding functionality

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultipleCourses.Models;
+
+namespace MultipleCourses.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base (options)
+        {    
+        }
+
+        public DbSet<RegistrationForm> registrationForms { get; set; }
+        public DbSet<Qualification> qualification {  get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RegistrationForm>()
+                .HasKey(h => h.Id);
+            modelBuilder.Entity<Qualification>()
+                .HasKey(r => r.QualificationId);
+
+            modelBuilder.Entity<Qualification>()
+                .HasOne(e => e.RegistrationForm)
+                .WithMany(r => r.qualifications)
+                .HasForeignKey(q => q.RegistrationFormId);
+        }
+    }
+}
---
+++ Models/RegistrationForm.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MultipleCourses.Models
+{
+    public class RegistrationForm
+    {
+        public RegistrationForm()
+        {
+            qualifications = new List<Qualification>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int? Age { get; set; }
+
+        [AtLeastOneQualificationRequired(ErrorMessage ="Atleast one qualification is required")]
+        public IList<Qualification> qualifications { get; set; }
+        
+    }
+    public class Qualification
+    {
+        [Key]
+        public int QualificationId { get; set; }
+        [Required(ErrorMessage ="Course name is required")]
+        public string CourseName { get; set; }
+        [Required(ErrorMessage = "Year of pass is required")]
+        public string YearofPass { get; set; }
+        [Required(ErrorMessage = "University is required")]
+        public string University { get; set; }
+        public int RegistrationFormId { get; set; }
+
+        public RegistrationForm RegistrationForm { get; set; }
+    }
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple =false)]
+    public class AtLeastOneQualificationRequired : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value is ICollection<Qualification> collection)
+            {
+                return collection.Count > 0;
+            }
+            return false;
+        }
+    }
+}
